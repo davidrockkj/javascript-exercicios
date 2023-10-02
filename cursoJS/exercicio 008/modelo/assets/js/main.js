531b90e84d07bce0355ff668d01efc910fe0fa5b
David Rocha <davidrocha@pop-os.localdomain>
2023-10-02 19:03:41 -0300

    Fazendo exercício com tudo que foi visto

---
+++ cursoJS/exercicio 008/modelo/assets/js/main.js
@@ -0,0 +1,13 @@
+function calculoIMC() {
+    const form = document.querySelector('.formDados');
+    const btnCalcular = document.querySelector('#btnCalcular');
+
+    function calcular() {
+        const peso = form.querySelector('#peso');
+        const altura = form.querySelector('#altura');
+
+        // if (!altura)
+    }
+
+    form.addEventListener('submit', calcular);
+}
